d6aca6640e26ba9d38d08ca0336b31b66ecb6a0d
SILI-LIAO <139749522+SILI-LIAO@users.noreply.github.com>
2023-10-10 22:00:48 +1300

    copying app

---
+++ connect.py
@@ -0,0 +1,7 @@
+# These details are available on the first MySQL Workbench screen
+# Usually called 'Local Instance'
+dbuser = "root" # Your MySQL username - likely 'root'
+dbpass = "871372Lesli." # ---- PUT YOUR PASSWORD HERE ----
+dbhost = "localhost" 
+dbport = "3306"
+dbname = "motorkhana"
